e9b5c8d594a59fb1ac8604e875a4228d567b4f40
Daniel Sardina <danielsardina@Daniels-Mac-mini.local>
2023-07-15 22:52:47 -0700

    Register, Home, and Login Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css"
 import Home from './pages/Home'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import Header from './componenets/Header'
 
 function App() {
   return <>
     <Router>
       <div className='container'>
+        <Header/>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
@@ -14,6 +18,7 @@ function App() {
         </Routes>
       </div>
     </Router>
+    <ToastContainer />
   </>
   
 }
--- frontend/src/componenets/Header.jsx
+++ frontend/src/componenets/Header.jsx
@@ -1,9 +1,25 @@
-import {FASignInAlt, FaSignOutAlt, FaUser} from 'react-icons'
+import {FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header className='header'>
+      <div className="logo">
+        <Link to='/'>Support Desk</Link>
+      </div>
+      <ul>
+        <li>
+          <Link to='/login'>
+            <FaSignInAlt /> Login
+          </Link>
+        </li>
+        <li>
+          <Link to='/register'>
+            <FaUser /> Register
+          </Link>
+        </li>
+      </ul>
+    </header>
   )
 }
 
